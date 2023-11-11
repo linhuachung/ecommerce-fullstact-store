0ab403d79d0d7dc09b2fac613d4b79bb78e4e2a7
Hua Chung <chunglh@bap.jp>
2023-11-11 12:28:04 +0700

    finally

--- app/(routes)/cart/page.tsx
+++ app/(routes)/cart/page.tsx
@@ -1,32 +1,38 @@
-'use client'
-import React, {useEffect, useState} from 'react';
-import Container from "@/components/ui/container";
-import useCart from "@/hooks/use-cart";
-import CartItem from "@/app/(routes)/cart/components/cart-item";
-import Summary from "@/app/(routes)/cart/components/summary";
+"use client";
+
+import {useEffect, useState} from 'react';
+
+import Container from '@/components/ui/container';
+import useCart from '@/hooks/use-cart';
+
+import Summary from './components/summary'
+import CartItem from './components/cart-item';
+
+export const revalidate = 0;
 
 const CartPage = () => {
-    const [isMounted, setIsMounted] = useState(false)
-    const cart = useCart()
+    const [isMounted, setIsMounted] = useState(false);
+    const cart = useCart();
 
-    useEffect(() => setIsMounted(true), [])
+    useEffect(() => {
+        setIsMounted(true);
+    }, []);
 
-    if (!isMounted) return null
+    if (!isMounted) {
+        return null;
+    }
 
     return (
-        <div className={"bg-white"}>
+        <div className="bg-white">
             <Container>
                 <div className="px-4 py-16 sm:px-6 lg:px-8">
                     <h1 className="text-3xl font-bold text-black">Shopping Cart</h1>
                     <div className="mt-12 lg:grid lg:grid-cols-12 lg:items-start gap-x-12">
-                        <div className={"lg:col-span-7"}>
-                            {cart.items.length === 0 && <p className="text-neutral-500">No items added to cart</p>}
+                        <div className="lg:col-span-7">
+                            {cart.items.length === 0 && <p className="text-neutral-500">No items added to cart.</p>}
                             <ul>
-                                {cart.items.map(item => (
-                                    <CartItem
-                                        key={item.id}
-                                        data={item}
-                                    />
+                                {cart.items.map((item) => (
+                                    <CartItem key={item.id} data={item}/>
                                 ))}
                             </ul>
                         </div>
@@ -34,7 +40,8 @@ const CartPage = () => {
                     </div>
                 </div>
             </Container>
-        </div>)
+        </div>
+    )
 };
 
 export default CartPage;
